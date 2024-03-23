3183d4d6ba8ee3c8d1b997dbc89ddce256d4711d
Deepika3345 <deepikapatidar484@gmail.com>
2024-03-24 00:23:37 +0530

    middleware changes

--- backend/routes/docRoutes.js
+++ backend/routes/docRoutes.js
@@ -1,5 +1,6 @@
 const express = require("express");
-const { authMiddleware } = require("../middleware/authmiddleware");
+const {authMiddleware}= require("../middleware/userMiddleware")
+
 const {
   createDoc,
   getAllDocs,
